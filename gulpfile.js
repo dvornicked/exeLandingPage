4b13c7b5226b3ba6f3c1cdcf7142f323074b02f7
Dvornick <mail@dvornick.ru>
2021-08-23 15:29:48 +0300

    Initialization

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+global.$ = {
+    gulp: require('gulp'),
+    gp: require('gulp-load-plugins')(),
+    sass: require('gulp-sass')(require('sass')),
+    browserSync: require('browser-sync').create(),
+    del: require('del'),
+    imageminJpegRecompress: require('imagemin-jpeg-recompress'),
+    pngquant: require('imagemin-pngquant'),
+    path: {
+        config: require('./gulp/config'),
+        jquery: './js/jquery.js',
+        js: './js/**/*.js',
+    }
+};
+
+$.path.config.forEach(function(path) {
+    require(path)();
+});
